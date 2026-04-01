8e4c177f0093f15733622714011cf67251f9b36c
Luong Nguyen <luong@onefusion.tech>
2026-04-01 11:20:13 +0700

    kudo 2

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
+  base: "/eng-kudo/",
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
